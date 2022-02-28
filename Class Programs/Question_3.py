51f30547799007f8521dbd24770d5b67bfe22d3a
Ishan Srivastava <58660320+ishan8604@users.noreply.github.com>
2022-02-28 23:13:10 +0530

    comment

--- Class Programs/Question_3.py
+++ Class Programs/Question_3.py
@@ -1,3 +1,4 @@
+#Write a program check whether last digit of number is 1 or 0
 x=int(input("Enter a number to check "))
 p=x%2
 print(p==1 and "1" or "0")
--- Class Programs/Question_6.py
+++ Class Programs/Question_6.py
@@ -1,3 +1,4 @@
+#Write a program to find out BMI
 w=float(input("Enter your weight "))
 h=float(input("Enter your height "))
 bmi=w*h*h
