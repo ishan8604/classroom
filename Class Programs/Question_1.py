31fca70626aaec60eafd0624c3f682d83994c3db
Ishan Srivastava <58660320+ishan8604@users.noreply.github.com>
2022-02-28 23:08:51 +0530

    comment

--- Class Programs/Question_1.py
+++ Class Programs/Question_1.py
@@ -1,3 +1,4 @@
+#Write a program in python to multiply a number by 32 using bitwise operator only
 x=int(input("Enter the number to be multiplied by 32"))
 p=(x<<5)
 print("%d multiplies by 32 is %d"%(x,p))
