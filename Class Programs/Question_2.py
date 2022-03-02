d331f9fa3895cbd73243f5a8b8bdb45008467841
Ishan Srivastava <58660320+ishan8604@users.noreply.github.com>
2022-03-03 01:02:24 +0530

    chnage 2

--- Class Programs/Question_2.py
+++
@@ -1,4 +0,0 @@
-#Write a program in python to multiply a number by 33 using bitwise operator only
-x=int(input("Enter the number to be multiplied by 33"))
-p=(x<<5)+x
-print("%d when multiplied by 33 gives %d"%(x,p))
--- Class Programs/Question_6.py
+++
@@ -1,5 +0,0 @@
-#Write a program to find out BMI
-w=float(input("Enter your weight "))
-h=float(input("Enter your height "))
-bmi=w*h*h
-print("Your BMI = %d"%bmi)
